8f271fd2705aaec6e2a8ae9b80ec1986197d6619
bladijacob <72238leo@universidaddeleon.edu.mx>
2026-01-26 08:46:31 -0600

    listas

---
+++ 05-lista.py
@@ -0,0 +1,14 @@
+'''
+una lista es una estructura de datos que permitealmacenar varios valores en una sola variable
+Las listas pueden contener datos del mismo o de distinto tipo y son mutables ( se pueden modificar)
+'''
+
+lista2=[]
+numeros=[1,2,3,4,5]
+print(numeros)
+nombres=["Ana","Luis","Carlos"]
+print(nombres)
+mezcla=[10,"Hola",3.5,True]
+print(mezcla)
+
+print(nombres[1])
